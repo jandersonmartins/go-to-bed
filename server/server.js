7064c319bbd73106c054d49a5786cc91b061b1a1
Janderson Martins <jandersonmartins10@gmail.com>
2020-03-21 20:19:56 -0300

    Configure eslint

--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
-const { join } = require('path');
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const cors = require('cors');
+const { join } = require('path')
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-let index = 0;
+let index = 0
 
-app.use(cors());
-app.use(fileUpload());
+app.use(cors())
+app.use(fileUpload())
 
-app.post('/photo', (req, res) => {
-  const fullPath = join(__dirname, 'photos', `${++index}.jpeg`);
-  req.files.photo.mv(fullPath, () => res.end());
-});
+app.post('/upload-photo', (req, res) => {
+  const fullPath = join(__dirname, 'photos', `${++index}.jpeg`)
+  req.files.photo.mv(fullPath, err => {
+    // ignoring error for now :)
+    res.end()
+  })
+})
 
 app.listen(3333, () => console.log('running'))
--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,11 @@
-import React, { useRef, useEffect } from 'react';
-
-import * as cocoSsd from "@tensorflow-models/coco-ssd";
-import "@tensorflow/tfjs";
+import React, { useRef, useEffect } from 'react'
+import '@tensorflow/tfjs'
+import * as cocoSsd from '@tensorflow-models/coco-ssd'
 import './App.css'
 
 const App = () => {
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
+  const videoRef = useRef(null)
+  const canvasRef = useRef(null)
 
   useEffect(() => {
     if (
@@ -17,98 +16,99 @@ const App = () => {
         .getUserMedia({
           audio: false,
           video: {
-            facingMode: "user"
+            facingMode: 'user'
           }
         })
         .then(stream => {
-          window.stream = stream;
-          videoRef.current.srcObject = stream;
+          window.stream = stream
+          videoRef.current.srcObject = stream
           return new Promise((resolve, reject) => {
             videoRef.current.onloadedmetadata = () => {
-              resolve();
-            };
-          });
-        });
+              resolve()
+            }
+          })
+        })
 
-      const modelPromise = cocoSsd.load();
+      const modelPromise = cocoSsd.load()
 
       Promise.all([modelPromise, webCamPromise])
         .then(values => {
-          detectFrame(videoRef.current, values[0]);
+          detectFrame(videoRef.current, values[0])
         })
         .catch(error => {
-          console.error(error);
-        });
+          console.error(error)
+        })
     }
   }, [])
 
   const detectFrame = (video, model) => {
     model.detect(video).then(predictions => {
-      renderPredictions(predictions);
+      renderPredictions(predictions)
       requestAnimationFrame(() => {
-        detectFrame(video, model);
-      });
-    });
-  };
+        detectFrame(video, model)
+      })
+    })
+  }
 
   const drawBox = (ctx, font, prediction) => {
-    const x = prediction.bbox[0];
-    const y = prediction.bbox[1];
-    const width = prediction.bbox[2];
-    const height = prediction.bbox[3];
+    const x = prediction.bbox[0]
+    const y = prediction.bbox[1]
+    const width = prediction.bbox[2]
+    const height = prediction.bbox[3]
     // Draw the bounding box.
-    ctx.strokeStyle = "#00FFFF";
-    ctx.lineWidth = 4;
-    ctx.strokeRect(x, y, width, height);
+    ctx.strokeStyle = '#00FFFF'
+    ctx.lineWidth = 4
+    ctx.strokeRect(x, y, width, height)
     // Draw the label background.
-    ctx.fillStyle = "#00FFFF";
-    const textWidth = ctx.measureText(prediction.class).width;
-    const textHeight = parseInt(font, 10); // base 10
-    ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+    ctx.fillStyle = '#00FFFF'
+    const textWidth = ctx.measureText(prediction.class).width
+    const textHeight = parseInt(font, 10) // base 10
+    ctx.fillRect(x, y, textWidth + 4, textHeight + 4)
   }
 
   const drawText = (ctx, prediction) => {
-    const x = prediction.bbox[0];
-    const y = prediction.bbox[1];
+    const x = prediction.bbox[0]
+    const y = prediction.bbox[1]
     // Draw the text last to ensure it's on top.
-    ctx.fillStyle = "#000000";
-    ctx.fillText(prediction.class, x, y);
+    ctx.fillStyle = '#000000'
+    ctx.fillText(prediction.class, x, y)
   }
 
   const renderPredictions = predictions => {
-    const video = videoRef.current;
-    const ctx = canvasRef.current.getContext('2d');
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    const video = videoRef.current
+    const ctx = canvasRef.current.getContext('2d')
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
     // Font options.
-    const font = '16px sans-serif';
-    ctx.font = font;
-    ctx.textBaseline = "top";
-    ctx.drawImage(video, 0, 0, video.width, video.height);
+    const font = '16px sans-serif'
+    ctx.font = font
+    ctx.textBaseline = 'top'
+    ctx.drawImage(video, 0, 0, video.width, video.height)
     predictions.forEach(prediction => {
-      drawBox(ctx, font, prediction);
-      drawText(ctx, prediction);
+      drawBox(ctx, font, prediction)
+      drawText(ctx, prediction)
 
       if (prediction.class === 'person') {
         upload(canvasRef.current)
       }
-    });
-  };
+    })
+  }
 
   const upload = canvas => {
     canvas.toBlob(blob => {
       const formData = new FormData()
       formData.append('photo', blob, Date.now().toString())
-      fetch('http://localhost:3333/photo', {
+
+      fetch('http://localhost:3333/upload-photo', {
         method: 'POST',
         body: formData
-      }).catch(() => {})
+      }).catch(() => { /* ignoring */ })
     }, 'image/jpeg', 0.95)
   }
 
   return (
     <div>
       <video
-        className="size"
+        className="video"
         autoPlay
         playsInline
         muted
@@ -117,13 +117,13 @@ const App = () => {
         height="500"
       />
       <canvas
-        className="size"
+        className="video"
         ref={canvasRef}
         width="600"
         height="500"
       />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
